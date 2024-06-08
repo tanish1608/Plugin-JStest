84346bde38d123a6436fa70f83864fbd398aaca3
GitHub Action <action@github.com>
2024-06-08 07:49:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/tanish1608/pluginjstest/MyBundle.kt
+++ src/main/kotlin/com/github/tanish1608/pluginjstest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.tanish1608.pluginjstest
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/tanish1608/pluginjstest/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/tanish1608/pluginjstest/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.tanish1608.pluginjstest.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
